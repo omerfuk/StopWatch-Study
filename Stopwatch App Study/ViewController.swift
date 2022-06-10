731fce1920223623cb0fe56ea7e20fe7b585089c
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-10 14:08:01 +0300

    .

--- Stopwatch App Study/ViewController.swift
+++ Stopwatch App Study/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
+    var time = 0
 
+    @IBOutlet weak var timeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func startButton(_ sender: Any) {
+        
+        timeLabel.text = String(time)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerApp), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func stopButton(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        time = 0
+        timeLabel.text = String(time)
+        timer.invalidate()
+    }
+    
+    @objc func timerApp() {
+        timeLabel.text = String(time)
+        time += 1
+    }
 }
 
